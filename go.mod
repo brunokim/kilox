49ab5dca4a25eb9cc0d06bc6a92ee6d8848a4f7c
Bruno Kim <brunokim.mc@gmail.com>
2022-06-22 16:45:50 -0300

    Unit tests for Scanner

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/brunokim/lox
 
 go 1.18
+
+require github.com/google/go-cmp v0.5.8 // indirect
---
+++ scanner_test.go
@@ -0,0 +1,99 @@
+package lox_test
+
+import (
+	"testing"
+
+	"github.com/brunokim/lox"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+)
+
+func token(tokenType lox.TokenType, text string) lox.Token {
+	return literal(tokenType, text, nil)
+}
+
+func literal(tokenType lox.TokenType, text string, literal interface{}) lox.Token {
+	return lox.Token{
+		TokenType: tokenType,
+		Lexeme:    text,
+		Literal:   literal,
+	}
+}
+
+func TestScanner(t *testing.T) {
+	tests := []struct {
+		text string
+		want []lox.Token
+	}{
+		{"a", []lox.Token{token(lox.Identifier, "a"), token(lox.EOF, "")}},
+		{" a", []lox.Token{token(lox.Identifier, "a"), token(lox.EOF, "")}},
+		{" a ", []lox.Token{token(lox.Identifier, "a"), token(lox.EOF, "")}},
+		{"a + 1", []lox.Token{
+			token(lox.Identifier, "a"),
+			token(lox.Plus, "+"),
+			literal(lox.Number, "1", 1.0),
+			token(lox.EOF, "")}},
+		{"!(x and false)", []lox.Token{
+			token(lox.Bang, "!"),
+			token(lox.LeftParen, "("),
+			token(lox.Identifier, "x"),
+			token(lox.And, "and"),
+			token(lox.False, "false"),
+			token(lox.RightParen, ")"),
+			token(lox.EOF, ""),
+		}},
+		{`"abc \" def \\ ghi"`, []lox.Token{
+			literal(lox.String, `"abc \" def \\ ghi"`, `abc " def \ ghi`),
+			token(lox.EOF, ""),
+		}},
+		{"\"abc\ndef\"", []lox.Token{
+			literal(lox.String, "\"abc\ndef\"", "abc\ndef"),
+			token(lox.EOF, ""),
+		}},
+	}
+
+	for _, test := range tests {
+		s := lox.NewScanner(test.text)
+		tokens, err := s.ScanTokens()
+		if err != nil {
+			t.Fatalf("want nil, got err: %v", err)
+		}
+		opts := cmp.Options{cmpopts.IgnoreFields(lox.Token{}, "Line")}
+		if d := cmp.Diff(test.want, tokens, opts); d != "" {
+			t.Errorf("(-want, +got)%s", d)
+		}
+	}
+}
+
+func TestScannerError(t *testing.T) {
+	tests := []struct {
+		text string
+		want string
+	}{
+		{"a % b", "line 1: unexpected character: %"},
+		{"a \n% \nb", "line 2: unexpected character: %"},
+		{`"unterminated`, "line 1: unterminated string"},
+		{`"unterminated`, "line 1: unterminated string"},
+		{`"unterminated
+
+        `, "line 3: unterminated string"},
+		{`"invalid \n escape"`, "line 1: invalid escaped character 'n' in string"},
+		{`a ^ b
+         "str\b
+         `, `multiple errors:
+  line 1: unexpected character: ^
+  line 2: invalid escaped character 'b' in string
+  line 3: unterminated string`},
+	}
+
+	for _, test := range tests {
+		s := lox.NewScanner(test.text)
+		tokens, err := s.ScanTokens()
+		if err == nil {
+			t.Fatalf("want err, got tokens %v for text %q", tokens, test.text)
+		}
+		if d := cmp.Diff(test.want, err.Error()); d != "" {
+			t.Errorf("(-want, +got)%s", d)
+		}
+	}
+}
