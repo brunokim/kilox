0e5b338327637cafeb0d9cccaedf9ca0625ae000
Bruno Kim <brunokim.mc@gmail.com>
2022-07-20 22:10:47 -0300

    Handling constraints during unification

--- type_unification.go
+++ type_unification.go
@@ -7,27 +7,28 @@ import (
 type typePair [2]Type
 
 type choicePoint struct {
-	options []Type
+	constraints []Constraint
 
-	topRefID  int
-	optionIdx int
-	t2        Type
-	trail     []*RefType
-	stack     []typePair
+	topRefID int
+	trail    []*RefType
+	stack    []typePair
+
+	x *RefType
+	t Type
 }
 
 // Interface to decouple unifier from type checker.
 type unificationCtx interface {
-	addErrors(errs ...typeError)
 	getRefID() int
 	setRefID(id int)
 }
 
 type unifier struct {
-	ctx     unificationCtx
-	stack   []typePair
-	choices []*choicePoint
-	errors  []typeError
+	ctx         unificationCtx
+	stack       []typePair
+	choices     []*choicePoint
+	errors      []typeError
+	constraints []Constraint
 
 	t2 Type
 }
@@ -36,18 +37,23 @@ func newUnifier(ctx unificationCtx) *unifier {
 	return &unifier{ctx: ctx}
 }
 
+func (u *unifier) reset() {
+	u.stack = nil
+	u.choices = nil
+	u.errors = nil
+	u.constraints = nil
+}
+
 func (u *unifier) push(t1, t2 Type) {
 	u.stack = append(u.stack, typePair{t1, t2})
 }
 
 func (u *unifier) err(t1, t2 Type) {
-	u.errors = append(u.errors, typeError{t1, t2})
-	if len(u.choices) > 0 {
-		u.backtrack()
-	} else {
-		u.ctx.addErrors(u.errors...)
-		u.errors = nil
+	if len(u.constraints) == 0 {
+		// Keep track of errors only if we don't have any solution.
+		u.errors = append(u.errors, typeError{t1, t2})
 	}
+	u.backtrack()
 }
 
 func (u *unifier) match(t1, t2 Type) {
@@ -55,58 +61,114 @@ func (u *unifier) match(t1, t2 Type) {
 	t1.accept(u)
 }
 
-func (u *unifier) unify(t1, t2 Type) {
+func (u *unifier) unify(t1, t2 Type) ([]Constraint, error) {
+	u.reset()
 	u.stack = []typePair{{t1, t2}}
-	for len(u.stack) > 0 {
-		n := len(u.stack)
-		var top typePair
-		top, u.stack = u.stack[n-1], u.stack[:n-1]
-		t1, t2 = deref(top[0]), deref(top[1])
-		if x1, ok := t1.(*RefType); ok {
-			u.match(x1, t2)
+	for len(u.stack) > 0 || len(u.choices) > 0 {
+		if len(u.stack) > 0 {
+			u.unifyStep()
 			continue
 		}
-		if x2, ok := t2.(*RefType); ok {
-			u.match(x2, t1)
-			continue
-		}
-		if _, ok := t2.(NilType); ok {
-			// Nil matches with anything.
-			// The case for t1.(NilType) is handled in its visitNilType method.
-			continue
-		}
-		u.match(t1, t2)
+		// Stack is empty, found a solution!
+		u.constraints = append(u.constraints, u.constraint())
+		u.errors = nil
+		u.backtrack()
 	}
+	if len(u.constraints) == 0 {
+		return nil, errors[typeError](u.errors)
+	}
+	if len(u.constraints) == 1 {
+		for x, value := range u.constraints[0] {
+			x.Value = value
+		}
+		return u.constraints, nil
+	}
+	// Delete existing constraints from refs.
+	for _, constraint := range u.constraints {
+		for x := range constraint {
+			x.constraints = nil
+		}
+	}
+	// Replace with constraint from unification.
+	for _, constraint := range u.constraints {
+		for x := range constraint {
+			x.constraints = append(x.constraints, constraint)
+		}
+	}
+	return u.constraints, nil
 }
 
-func (u *unifier) pushChoicePoint(options []Type) {
+func (u *unifier) constraint() Constraint {
+	constraint := make(Constraint)
+	for _, choice := range u.choices {
+		for _, x := range choice.trail {
+			constraint[x] = x.Value
+		}
+	}
+	return constraint
+}
+
+func (u *unifier) unifyStep() {
+	n := len(u.stack)
+	var top typePair
+	top, u.stack = u.stack[n-1], u.stack[:n-1]
+	t1, t2 := deref(top[0]), deref(top[1])
+	if x1, ok := t1.(*RefType); ok {
+		u.match(x1, t2)
+		return
+	}
+	if x2, ok := t2.(*RefType); ok {
+		u.match(x2, t1)
+		return
+	}
+	if _, ok := t2.(NilType); ok {
+		// Nil matches with anything.
+		// The case for t1.(NilType) is handled in its visitNilType method.
+		return
+	}
+	u.match(t1, t2)
+}
+
+func (u *unifier) pushChoicePoint(x *RefType, constraints []Constraint) {
 	// TODO: split stack in environments so that we don't need to copy everything?
 	stack := make([]typePair, len(u.stack))
 	copy(stack, u.stack)
 
 	choice := &choicePoint{
-		options:  options,
-		topRefID: u.ctx.getRefID(),
-		stack:    stack,
-		t2:       u.t2,
+		constraints: constraints,
+		topRefID:    u.ctx.getRefID(),
+		stack:       stack,
+		x:           x,
+		t:           u.t2,
 	}
 	u.choices = append(u.choices, choice)
 }
 
-func (u *unifier) backtrack() {
-	n := len(u.choices)
-	if n == 0 {
-		panic("no more choices")
+func (u *unifier) popConstraint() (*choicePoint, Constraint) {
+	for i := len(u.choices) - 1; i >= 0; i-- {
+		choice := u.choices[i]
+		choice.unwindTrail()
+		if len(choice.constraints) > 0 {
+			// Pop constraint from choicepoint.
+			constraint := choice.constraints[0]
+			choice.constraints = choice.constraints[1:]
+			return choice, constraint
+		}
+		// Pop choicepoint if it has no more constraints.
+		u.choices = u.choices[:i]
 	}
-	u.unwindTrail()
-	choice := u.choices[n-1]
-	choice.optionIdx++
-	// If this is the last option, pop choice point.
-	if choice.optionIdx == len(choice.options)-1 {
-		u.choices = u.choices[:n-1]
+	return nil, nil
+}
+
+func (u *unifier) backtrack() error {
+	choice, constraint := u.popConstraint()
+	if choice == nil {
+		return fmt.Errorf("no more choices")
 	}
-	option := choice.options[choice.optionIdx]
+
+	// Reset ctx state.
 	u.ctx.setRefID(choice.topRefID)
+
 	// Reset stack.
 	l1, l2 := len(u.stack), len(choice.stack)
 	copy(u.stack, choice.stack)
@@ -115,8 +177,42 @@ func (u *unifier) backtrack() {
 	} else {
 		u.stack = u.stack[:l2]
 	}
-	// Push new option.
-	u.push(option, choice.t2)
+
+	// Set new constraints.
+	u.applyConstraint(choice.x, choice.t, constraint)
+
+	return nil
+}
+
+func (u *unifier) peek() *choicePoint {
+	n := len(u.choices)
+	if n == 0 {
+		return nil
+	}
+	return u.choices[n-1]
+}
+
+func (u *unifier) unifyRef(x *RefType, t Type) {
+	if len(x.constraints) == 0 {
+		u.bindRef(x, t)
+		return
+	}
+	constraint := x.constraints[0]
+	u.pushChoicePoint(x, x.constraints[1:])
+	u.applyConstraint(x, t, constraint)
+}
+
+func (u *unifier) applyConstraint(x *RefType, t Type, constraint Constraint) {
+	for y, value := range constraint {
+		u.bindRef(y, value)
+	}
+	if x.Value == nil {
+		// Variable constraints do not apply to itself (is this legal?).
+		u.bindRef(x, t)
+	} else {
+		// Revisit x, now bound to a constraint.
+		u.push(x, t)
+	}
 }
 
 func (u *unifier) bindRef(x *RefType, t Type) {
@@ -124,35 +220,31 @@ func (u *unifier) bindRef(x *RefType, t Type) {
 		panic(fmt.Sprintf("compiler error: expecting to be called on an unbound ref, got %v", TypePrint(x)))
 	}
 	x.Value = t
-	u.trail(x)
+	u.peek().addToTrail(x)
 }
 
 // Adds x to the trail, indicating that it was bound during the current choice point.
-func (u *unifier) trail(x *RefType) {
-	n := len(u.choices)
-	if n == 0 {
+func (cp *choicePoint) addToTrail(x *RefType) {
+	if cp == nil {
 		return
 	}
-	choice := u.choices[n-1]
-	if choice.topRefID < x.id {
+	if cp.topRefID < x.id {
 		// Unconditional ref: x is newer than current choice point, so it will
 		// be recreated if we backtrack. There's no need to add it to the trail.
 		return
 	}
-	choice.trail = append(choice.trail, x)
+	cp.trail = append(cp.trail, x)
 }
 
 // Reset bindings for all refs that were bound during the current choice point.
-func (u *unifier) unwindTrail() {
-	n := len(u.choices)
-	if n == 0 {
+func (cp *choicePoint) unwindTrail() {
+	if cp == nil {
 		return
 	}
-	choice := u.choices[n-1]
-	for _, x := range choice.trail {
+	for _, x := range cp.trail {
 		x.Value = nil
 	}
-	choice.trail = nil
+	cp.trail = nil
 }
 
 // ----
@@ -198,12 +290,12 @@ func (u *unifier) visitFunctionType(t1 FunctionType) {
 func (u *unifier) visitRefType(x *RefType) {
 	y, ok := u.t2.(*RefType)
 	if !ok {
-		u.bindRef(x, u.t2)
+		u.unifyRef(x, u.t2)
 		return
 	}
 	if x.id < y.id {
-		u.bindRef(y, x)
+		u.unifyRef(y, x)
 	} else if x.id > y.id {
-		u.bindRef(x, y)
+		u.unifyRef(x, y)
 	}
 }
