e7a6fa9de569cbae01b1d162c02a36897f525107
Bruno Kim <brunokim.mc@gmail.com>
2022-07-23 20:15:24 -0300

    Move AST String to ast.go

--- ast.go
+++ ast.go
@@ -3,3 +3,36 @@ package lox
 //go:generate go run ./cmd/gen_ast -spec ./cmd/gen_ast/expr.spec -dest expr.go -extensions typename
 //go:generate go run ./cmd/gen_ast -spec ./cmd/gen_ast/stmt.spec -dest stmt.go
 //go:generate go run ./cmd/gen_ast -spec ./cmd/gen_ast/type.spec -dest type.go
+
+// ---- String
+
+func (e *BinaryExpr) String() string     { return PrintExpr(e) }
+func (e *GroupingExpr) String() string   { return PrintExpr(e) }
+func (e *LiteralExpr) String() string    { return PrintExpr(e) }
+func (e *UnaryExpr) String() string      { return PrintExpr(e) }
+func (e *VariableExpr) String() string   { return PrintExpr(e) }
+func (e *AssignmentExpr) String() string { return PrintExpr(e) }
+func (e *CallExpr) String() string       { return PrintExpr(e) }
+func (e *FunctionExpr) String() string   { return PrintExpr(e) }
+func (e *GetExpr) String() string        { return PrintExpr(e) }
+func (e *SetExpr) String() string        { return PrintExpr(e) }
+func (e *ThisExpr) String() string       { return PrintExpr(e) }
+
+func (s ExpressionStmt) String() string { return PrintStmts(s) }
+func (s PrintStmt) String() string      { return PrintStmts(s) }
+func (s VarStmt) String() string        { return PrintStmts(s) }
+func (s IfStmt) String() string         { return PrintStmts(s) }
+func (s BlockStmt) String() string      { return PrintStmts(s) }
+func (s LoopStmt) String() string       { return PrintStmts(s) }
+func (s BreakStmt) String() string      { return PrintStmts(s) }
+func (s ContinueStmt) String() string   { return PrintStmts(s) }
+func (s FunctionStmt) String() string   { return PrintStmts(s) }
+func (s ReturnStmt) String() string     { return PrintStmts(s) }
+func (s ClassStmt) String() string      { return PrintStmts(s) }
+
+func (t NilType) String() string      { return PrintType(t) }
+func (t BoolType) String() string     { return PrintType(t) }
+func (t NumberType) String() string   { return PrintType(t) }
+func (t StringType) String() string   { return PrintType(t) }
+func (t FunctionType) String() string { return PrintType(t) }
+func (t *RefType) String() string     { return PrintType(t) }
--- ast_printer.go
+++ ast_printer.go
@@ -46,39 +46,6 @@ func PrintType(t Type) string {
 	return p.str.String()
 }
 
-// ---- String
-
-func (e *BinaryExpr) String() string     { return PrintExpr(e) }
-func (e *GroupingExpr) String() string   { return PrintExpr(e) }
-func (e *LiteralExpr) String() string    { return PrintExpr(e) }
-func (e *UnaryExpr) String() string      { return PrintExpr(e) }
-func (e *VariableExpr) String() string   { return PrintExpr(e) }
-func (e *AssignmentExpr) String() string { return PrintExpr(e) }
-func (e *CallExpr) String() string       { return PrintExpr(e) }
-func (e *FunctionExpr) String() string   { return PrintExpr(e) }
-func (e *GetExpr) String() string        { return PrintExpr(e) }
-func (e *SetExpr) String() string        { return PrintExpr(e) }
-func (e *ThisExpr) String() string       { return PrintExpr(e) }
-
-func (s ExpressionStmt) String() string { return PrintStmts(s) }
-func (s PrintStmt) String() string      { return PrintStmts(s) }
-func (s VarStmt) String() string        { return PrintStmts(s) }
-func (s IfStmt) String() string         { return PrintStmts(s) }
-func (s BlockStmt) String() string      { return PrintStmts(s) }
-func (s LoopStmt) String() string       { return PrintStmts(s) }
-func (s BreakStmt) String() string      { return PrintStmts(s) }
-func (s ContinueStmt) String() string   { return PrintStmts(s) }
-func (s FunctionStmt) String() string   { return PrintStmts(s) }
-func (s ReturnStmt) String() string     { return PrintStmts(s) }
-func (s ClassStmt) String() string      { return PrintStmts(s) }
-
-func (t NilType) String() string      { return PrintType(t) }
-func (t BoolType) String() string     { return PrintType(t) }
-func (t NumberType) String() string   { return PrintType(t) }
-func (t StringType) String() string   { return PrintType(t) }
-func (t FunctionType) String() string { return PrintType(t) }
-func (t *RefType) String() string     { return PrintType(t) }
-
 // ---- Expr
 
 func (p *astPrinter) visitBinaryExpr(expr *BinaryExpr) {
