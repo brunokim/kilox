61bdb39b1f939b20f5121d80c884018e0aeebdb4
Bruno Kim <brunokim.mc@gmail.com>
2022-08-21 19:48:53 -0300

    Remove doc from clause tests, which are just confusing

--- typing/clauses_test.go
+++ typing/clauses_test.go
@@ -12,13 +12,10 @@ import (
 func TestBuildClauses(t *testing.T) {
 	tests := []struct {
 		text string
-		doc  string
 		want []typing.TypeClause
 	}{
 		{
 			"fun foo() {}",
-			`Type("foo", Fun([], ret)) :-
-               ret = Nil.`,
 			clauses_(clause_(1, "foo", func_(types_(), refi_(6)),
 				binding_(refi_(6), nil_))),
 		},
@@ -27,10 +24,6 @@ func TestBuildClauses(t *testing.T) {
             fun foo() {}
             fun bar() {}
             `),
-			`Type("foo", Fun([], ret)) :-
-               ret = Nil.
-            Type("bar", Fun([], ret)) :-
-               ret = Nil.`,
 			clauses_(
 				clause_(1, "foo", func_(types_(), refi_(6)),
 					binding_(refi_(6), nil_)),
@@ -39,8 +32,6 @@ func TestBuildClauses(t *testing.T) {
 		},
 		{
 			"fun answer() { return 42; }",
-			`Type("answer", Fun([], ret)) :-
-               ret = Number.`,
 			clauses_(clause_(1, "answer", func_(types_(), refi_(6)),
 				binding_(refi_(6), num_))),
 		},
@@ -50,9 +41,6 @@ func TestBuildClauses(t *testing.T) {
               var a = "test";
               return a;
             }`),
-			`Type("foo", Fun([], ret)) :-
-               _a = String, % implicit
-               ret = _a.`,
 			clauses_(clause_(1, "foo", func_(types_(), refi_(6)),
 				binding_(refi_(6), brefi_(7, str_)))),
 		},
@@ -62,17 +50,11 @@ func TestBuildClauses(t *testing.T) {
                 foo;
                 print foo;
             }`),
-			`Type("foo", Fun([], ret)) :-
-               Type("foo", r1),
-               Type("foo", r2),
-               ret = Nil.`,
 			clauses_(clause_(1, "foo", func_(types_(), refi_(6)),
 				binding_(refi_(6), nil_))),
 		},
 		{
 			"fun id(x) { return x; }",
-			`Type("id", Fun([_x], ret)) :-
-               ret = _x.`,
 			clauses_(clause_(1, "id", func_(types_(refi_(6)), refi_(7)),
 				binding_(refi_(7), refi_(6)))),
 		},
@@ -83,9 +65,6 @@ func TestBuildClauses(t *testing.T) {
               a = x;
               return a;
             }`),
-			`Type("f", Fun([_x], ret)) :-
-               _a = _x,
-               ret = _a.`,
 			clauses_(clause_(1, "f", func_(types_(refi_(6)), refi_(7)),
 				binding_(refi_(8), refi_(6)),
 				binding_(refi_(7), refi_(8)))),
@@ -97,10 +76,6 @@ func TestBuildClauses(t *testing.T) {
               a = 20;
               return a = x;
             }`),
-			`Type("f", Fun([_x], ret)) :-
-               _a = Number,
-               _a = _x,
-               ret = _x.`,
 			clauses_(clause_(1, "f", func_(types_(refi_(6)), refi_(7)),
 				binding_(refi_(8), num_),
 				binding_(refi_(8), refi_(6)),
@@ -113,10 +88,6 @@ func TestBuildClauses(t *testing.T) {
               a = "str";
               return a;
             }`),
-			`Type("foo", Fun([], ret)) :-
-               _a = Number, % implicit
-               _a = String,
-               ret = _a.`,
 			clauses_(clause_(1, "foo", func_(types_(), refi_(6)),
 				binding_(brefi_(7, num_), str_),
 				binding_(refi_(6), brefi_(7, num_)))),
@@ -127,10 +98,6 @@ func TestBuildClauses(t *testing.T) {
                 var a = 43;
                 return f(a, x);
             }`),
-			`Type("callWith43", Fun([_f, _x], ret)) :-
-               _a = Number, % implicit
-               _f = Fun([_a, _x], r1),
-               ret = r1.`,
 			clauses_(clause_(1, "callWith43", func_(types_(refi_(6), refi_(7)), refi_(8)),
 				unify_(refi_(6), func_(types_(brefi_(9, num_), refi_(7)), refi_(10))),
 				binding_(refi_(8), refi_(10)))),
@@ -147,24 +114,6 @@ func TestBuildClauses(t *testing.T) {
             fun l_30() { return 30; }
             fun main() { print l_if(l_false, l_20, l_30); }
             `),
-			`Type("l_true", Fun([_x, _y], ret)) :-
-               ret = _x.
-             Type("l_false", Fun([_x, _y], ret)) :-
-               ret = _y.
-             Type("l_if", Fun([_cond, _then, _else], ret)) :-
-               _cond = Fun([_then, _else], r1),
-               r1 = Fun([], r2),
-               ret = r2.
-             Type("l_20", Fun([], ret)) :-
-               ret = Number.
-             Type("l_30", Fun([], ret)) :-
-               ret = Number.
-             Type("main", Fun([], ret)) :-
-               Type("l_if", Fun([_l_false, _l_20, _l_30], r1)),
-               Type("l_false", _l_false),
-               Type("l_20", _l_20),
-               Type("l_30", _l_30),
-               ret = r1.`,
 			clauses_(
 				clause_(1, "l_true", func_(types_(refi_(6), refi_(7)), refi_(8)),
 					binding_(refi_(8), refi_(6))),
@@ -192,10 +141,6 @@ func TestBuildClauses(t *testing.T) {
 		},
 		{
 			`fun foo() { return "a" + "b"; }`,
-			`Type("foo", Fun([], ret)) :-
-               Type("+", _plus),
-               _plus = Fun([String, String], r1),
-               ret = r1.`,
 			clauses_(clause_(1, "foo", func_(types_(), refi_(6)),
 				unify_(
 					brefi_(1, func_(types_(num_, num_), num_)),
@@ -212,15 +157,6 @@ func TestBuildClauses(t *testing.T) {
               a = 20;
               inner();
             }`),
-			`
-            :- dynamic _a.
-            Type("outer", Fun([], ret)) :-
-              _a = Number, % implicit
-              _a = Number,
-              Type("inner", Fun([], r1)),
-              ret = Nil.
-            Type("inner", Fun([], ret)) :-
-              ret = _a.`,
 			clauses_(
 				clause_(2, "inner", func_(types_(), refi_(9)),
 					binding_(refi_(9), brefi_(7, num_))),
@@ -243,29 +179,6 @@ func TestBuildClauses(t *testing.T) {
             }
             fun h() { return 40; }
             `),
-			`
-            Type("f1", Fun([], ret)) :-
-              Type("f1/g", r1),
-              r1 = Fun([], r2),
-              ret = r2.
-            Type("f1/g", Fun([], ret)) :-
-              ret = Number.,
-
-            Type("g", Fun([], ret)) :-
-              ret = Number.
-
-            :- dynamic _a.
-            Type("f2", Fun([], ret)) :-
-              Type("h", r1),
-              _a = r1, % implicit
-              Type("f2/g", r1),
-              r1 = Fun([], r2),
-              ret = r2.
-            Type("f2/g", Fun([], ret)) :-
-              ret = _a.
-
-            Type("h", Fun([], ret)) :-
-              ret = Number.`,
 			clauses_(
 				clause_(2, "g", func_(types_(), refi_(8)),
 					binding_(refi_(8), num_)),
@@ -285,27 +198,26 @@ func TestBuildClauses(t *testing.T) {
 					binding_(refi_(21), num_)),
 			),
 		},
-        {
-            dedent.Dedent(`
+		{
+			dedent.Dedent(`
             fun f() { g(); h(); }
             fun g() { h(); }
             fun h() {}`),
-            ``,
-            clauses_(
-                clause_(1, "f", func_(types_(), refi_(6)),
-                    call_(2, refi_(7)),
-                    unify_(refi_(7), func_(types_(), refi_(8))),
-                    call_(3, refi_(9)),
-                    unify_(refi_(9), func_(types_(), refi_(10))),
-                    binding_(refi_(6), nil_)),
-                clause_(2, "g", func_(types_(), refi_(12)),
-                    call_(3, refi_(13)),
-                    unify_(refi_(13), func_(types_(), refi_(14))),
-                    binding_(refi_(12), nil_)),
-                clause_(3, "h", func_(types_(), refi_(16)),
-                    binding_(refi_(16), nil_)),
-            ),
-        },
+			clauses_(
+				clause_(1, "f", func_(types_(), refi_(6)),
+					call_(2, refi_(7)),
+					unify_(refi_(7), func_(types_(), refi_(8))),
+					call_(3, refi_(9)),
+					unify_(refi_(9), func_(types_(), refi_(10))),
+					binding_(refi_(6), nil_)),
+				clause_(2, "g", func_(types_(), refi_(12)),
+					call_(3, refi_(13)),
+					unify_(refi_(13), func_(types_(), refi_(14))),
+					binding_(refi_(12), nil_)),
+				clause_(3, "h", func_(types_(), refi_(16)),
+					binding_(refi_(16), nil_)),
+			),
+		},
 	}
 	for _, test := range tests {
 		t.Run(test.text, func(t *testing.T) {
