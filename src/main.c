727aa99c658fdd8229d4de88f3d52cbb85839c97
Bruno Kim <brunokim.mc@gmail.com>
2022-12-29 01:29:06 -0300

    Implement actual stack pushing operations

--- src/main.c
+++ src/main.c
@@ -1,21 +1,25 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 #include <stdio.h>
 
 int main(int argc, const char *argv[]) {
+    initVM();
     printf("hello clox\n");
 
     Chunk chunk;
     initChunk(&chunk);
-    writeChunk(&chunk, OP_RETURN, 123);
 
     for (int i = 0; i < 260; i++) {
         writeConstant(&chunk, 1.0 * i + 0.5, 123);
     }
+    writeChunk(&chunk, OP_RETURN, 123);
 
     disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
 
     return 0;
---
+++ src/vm.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+
+#include "common.h"
+#include "vm.h"
+#include "chunk.h"
+#include "debug.h"
+#include "value.h"
+
+VM vm;
+
+static void resetStack() {
+    vm.stackTop = vm.stack;
+}
+
+void initVM() {
+    resetStack();
+}
+
+void freeVM() {
+}
+
+void push(Value value) {
+    *vm.stackTop = value;
+    vm.stackTop++;
+}
+
+Value pop() {
+    vm.stackTop--;
+    return *vm.stackTop;
+}
+
+static InterpretResult run() {
+#define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define READ_UINT24() ((READ_BYTE() << 0) | (READ_BYTE() << 8) | (READ_BYTE() << 16))
+#define READ_CONSTANT_LONG() (vm.chunk->constants.values[READ_UINT24()])
+
+    for (;;) {
+#ifdef DEBUG_TRACE_EXECUTION
+        printf("        ");
+        for (Value *slot = vm.stack; slot < vm.stackTop; slot++) {
+            printf("[ ");
+            printValue(*slot);
+            printf(" ]");
+        }
+        printf("\n");
+        disassembleInstruction(
+            vm.chunk,
+            (int)(vm.ip - vm.chunk->code));
+#endif
+        uint8_t instruction;
+        switch (instruction = READ_BYTE()) {
+            case OP_CONSTANT: {
+                Value constant = READ_CONSTANT();
+                push(constant);
+                break;
+            }
+            case OP_CONSTANT_LONG: {
+                Value constant = READ_CONSTANT_LONG();
+                push(constant);
+                break;
+            }
+            case OP_RETURN:
+                printValue(pop());
+                printf("\n");
+                return INTERPRET_OK;
+        }
+    }
+
+#undef READ_CONSTANT_LONG
+#undef READ_UINT24
+#undef READ_CONSTANT
+#undef READ_BYTE
+}
+
+InterpretResult interpret(Chunk *chunk) {
+    vm.chunk = chunk;
+    vm.ip = vm.chunk->code;
+    return run();
+}
