7d4e221d92ee0a5b69c511224562c95623ba19ab
Bruno Kim <brunokim.mc@gmail.com>
2023-01-02 00:58:31 -0300

    String concatenation

--- src/vm.c
+++ src/vm.c
@@ -1,12 +1,15 @@
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
 
+#include "chunk.h"
 #include "common.h"
 #include "compiler.h"
-#include "vm.h"
-#include "chunk.h"
 #include "debug.h"
+#include "memory.h"
+#include "object.h"
 #include "value.h"
+#include "vm.h"
 
 VM vm;
 
@@ -50,6 +53,20 @@ Value peek(int distance) {
 
 static bool isFalsey(Value value) {
     return IS_NIL(value) || IS_BOOL(value) && !AS_BOOL(value);
+}
+
+static void concatenate() {
+    ObjString *b = AS_STRING(pop());
+    ObjString *a = AS_STRING(pop());
+
+    int length = a->length + b->length;
+    char *chars = ALLOCATE(char, length + 1);
+    memcpy(chars, a->chars, a->length);
+    memcpy(chars + a->length, b->chars, b->length);
+    chars[length] = '\0';
+
+    ObjString *result = takeString(chars, length);
+    push(OBJ_VAL(result));
 }
 
 static InterpretResult run() {
@@ -109,7 +126,17 @@ static InterpretResult run() {
                 BINARY_OP(BOOL_VAL, <);
                 break;
             case OP_ADD:
-                BINARY_OP(NUMBER_VAL, +);
+                if (IS_STRING(peek(0)) && IS_STRING(peek(1))) {
+                    concatenate();
+                } else if (IS_NUMBER(peek(0)) && IS_NUMBER(peek(1))) {
+                    double b = AS_NUMBER(pop());
+                    double a = AS_NUMBER(pop());
+                    push(NUMBER_VAL(a + b));
+                } else {
+                    runtimeError(
+                        "Operands must be two numbers or two strings.");
+                    return INTERPRET_RUNTIME_ERROR;
+                }
                 break;
             case OP_SUBTRACT:
                 BINARY_OP(NUMBER_VAL, -);
